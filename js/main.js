08362cb7be6e3cccc6a96eae09481d2ad5971469
Jordan Alexander Cruz - AlexCG Design <jordanalexanderweb@gmail.com>
2021-10-11 11:25:41 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
